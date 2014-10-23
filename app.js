e8f3f97e2de0849f2a07b49edf0d84ce70d29e0b
Marta <Marta Fonda>
2014-10-23 16:38:02 +0100

    More than one object

--- app.js
+++ app.js
@@ -3,15 +3,23 @@
 var app = angular.module('store', [ ]);
 
 app.controller('StoreController', function(){
-	this.product = gem;
+	this.products = gems;
 });
 
-var gem = {
-	name: "Dodecadron",
-	price: 2.95,
-	description: "This is the best gem ever.",
-	canPurchase: false,
-	soldOut: false
-}
-
+var gems = [
+	{
+		name: "Dodecadron",
+		price: 2.95,
+		description: "This is the best gem ever.",
+		canPurchase: false,
+		soldOut: false
+	},
+	{
+		name: "Pentagonal Gem",
+		price: 5.95,
+		description: "Or this?? It's bigger",
+		canPurchase: true,
+		soldOut: false
+	}
+]
 })();
