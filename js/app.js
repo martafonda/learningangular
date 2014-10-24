1da4dc16f1a3e7c9139f7182cb870bdf39e747b3
Marta <Marta Fonda>
2014-10-24 11:11:16 +0100

    Refactoring Forms: ng-submit and controller usage

--- js/app.js
+++ js/app.js
@@ -16,6 +16,14 @@ app.controller('PanelController', function(){
 	};
 });
 
+app.controller('ReviewController', function(){
+	this.review = {};
+	this.addReview = function(product){
+		product.reviews.push(this.review);
+		this.review = {};
+	};
+});
+
 var gems = [
 	{
 		name: "Dodecahedron",
@@ -26,7 +34,20 @@ var gems = [
 			thumbs: ["images/gem3.png", "images/gem5.png"]
 		},
 		canPurchase: true,
-		soldOut: false
+		soldOut: false,
+		reviews: [
+			{
+	        	stars: 5,
+		        body: "I love this gem!",
+		        author: "joe@example.org",
+		        createdOn: 1397490980837
+	    	}, {
+		        stars: 1,
+		        body: "This gem sucks.",
+		        author: "tim@example.org",
+		        createdOn: 1397490980837
+	    	}
+		]
 	},
 	{
 		name: "Other Gem",
@@ -37,7 +58,21 @@ var gems = [
 			thumbs:["images/gem1.png", "images/gem4.png"]
 		},	
 		canPurchase: true,
-		soldOut: false
+		soldOut: false,
+		reviews: [
+			{
+		        stars: 3,
+		        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+		        author: "JimmyDean@example.org",
+		        createdOn: 1397490980837
+	      	}, {
+		        stars: 4,
+		        body: "Any gem with 12 faces is for me!",
+		        author: "gemsRock@example.org",
+		        createdOn: 1397490980837
+		    }
+
+		]
 	}
 ]
 })();
